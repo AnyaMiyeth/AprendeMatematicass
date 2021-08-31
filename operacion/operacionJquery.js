3d734308c7036d82797de8f6f89de326a1a27ce0
anyamiyeth <anyamiyeth@byasystems.com.co>
2021-08-30 23:02:07 -0500

    Jquery

---
+++ operacion/operacionJquery.js
@@ -0,0 +1,26 @@
+
+
+$(document).ready(function () {
+    $("#btnOperacion").click(function () {
+        op = $("#operacionSelccionada").val();
+        alert(op);
+        switch (op) {
+            case "suma":
+                var numero1 = $("#numero1").val();
+                var numero2 = $("#numero2").val();
+                var resultado = Number(numero1) + Number(numero2);
+                $("#resultado").val(resultado);
+                alert(resultado);
+                $("#btnOperacion").addClass("redondo");
+                break;
+            case "resta":
+                var numero1 = $("#numero1").val();
+                var numero2 = $("#numero2").val();
+                var resultado = Number(numero1) - Number(numero2);
+                $("#resultado").val(resultado);
+                alert(resultado);
+                break;
+        }
+    })
+
+});
